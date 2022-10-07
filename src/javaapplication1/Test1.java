071667da64af5bcbec96433752b92081599f0762
andrijant <andrija_ds@yahoo.com>
2022-10-07 13:55:00 +0200

    age defference calculation

--- src/javaapplication1/Test1.java
+++ src/javaapplication1/Test1.java
@@ -9,18 +9,27 @@ public class Test1 {
         birthYearOfStudent = 1988; // definition
         birthYearOfTeacher = 1981;
         
-        double pi = 3.1415; // double stores real decimal numbers
+        int ageDifference = birthYearOfStudent - birthYearOfTeacher;
         
-        boolean passedExam = true; // or 'false'
+        String message = "Age defference is " + ageDifference + ".";
         
-        char letter = 'M';  // single character/letter or ASCII values
+        System.out.println(message);
         
-        String studentsName;  // begins with capital letter 
-        String studentsLastName;
-        
-        studentsName = "John"; // String can be a number within double quotes "5"
-        studentsLastName = "Smith";
-        
+//        System.out.print("Age difference is: ");
+//        System.out.println(ageDifference);
+
     }
     
 }
+//        double pi = 3.1415; // double stores real decimal numbers
+        
+//        boolean passedExam = true; // or 'false'
+        
+//        char letter = 'M';  // single character/letter or ASCII values
+        
+//        String studentsName;  // begins with capital letter 
+//        String studentsLastName;
+        
+//        studentsName = "John"; // String can be a number within double quotes "5"
+//        studentsLastName = "Smith";
+        
