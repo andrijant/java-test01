633aceae58e02cf8c86dcf469dfcd90162070caa
andrijant <andrija_ds@yahoo.com>
2022-10-07 12:58:45 +0200

    first commit

---
+++ src/javaapplication1/Test1.java
@@ -0,0 +1,26 @@
+package javaapplication1;
+
+public class Test1 {
+    public static void main(String[] args) {
+        
+        int birthYearOfStudent; // declaration statement always ends with ';'
+        int birthYearOfTeacher; // int stores whole numbers
+        
+        birthYearOfStudent = 1988; // definition
+        birthYearOfTeacher = 1981;
+        
+        double pi = 3.1415; // double stores real decimal numbers
+        
+        boolean passedExam = true; // or 'false'
+        
+        char letter = 'M';  // single character/letter or ASCII values
+        
+        String studentsName;  // begins with capital letter 
+        String studentsLastName;
+        
+        studentsName = "John"; // String can be a number within double quotes "5"
+        studentsLastName = "Smith";
+        
+    }
+    
+}
