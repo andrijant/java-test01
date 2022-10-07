6941d84db112e875fab4dd5fed59fa8dec1e79fb
andrijant <andrija_ds@yahoo.com>
2022-10-07 14:27:17 +0200

    basic arithmetic operations

---
+++ src/javaapplication2/Test2.java
@@ -0,0 +1,30 @@
+package javaapplication2;
+
+public class Test2 {
+
+    public static void main(String[] args) {
+     
+        int a = 10;
+        int b = 3;
+        int c;
+        
+        c = a * b;
+        
+        System.out.println(c);
+        
+        c = a * a + 5;
+        
+        System.out.println(c);
+        
+        c = a / b;
+        
+        System.out.println(c);
+        
+        double d = 1.0 * a / b;
+        
+        System.out.println(d);
+        
+        
+    }
+    
+}
